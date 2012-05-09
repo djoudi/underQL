3713bc1a32214ba584c2d9451e01105bcfaecaf0
Abdullah Almehmadi <cs.almehmadi@gmail.com>
2012-05-09 03:13:17 +0300

    Add __construct to UQAbstractEntity

--- UQLAbstractEntity.php
+++ UQLAbstractEntity.php
@@ -1,17 +1,25 @@
 <?php
 
 class UQLAbstractEntity {
-	
+
 	private $entity_name;
 	private $fields;
 	private $fields_count;
-	
-	
-	public function __construct($entity_name,$database_handle)
-	{
-		
+
+	public function __construct($entity_name, $database_handle) {
+
+		if (($database_handle instanceof UQLConnection)) {
+			$this -> entity_name = $entity_name;
+			$local_string_query = sprintf("SHOW COLUMNS FROM `%s`",$this->entity_name);
+			$local_query_result = mysql_query($local_string_query,$database_handle->getDatabaseHandle());
+			if($local_query_result)
+			{
+				$this->fields_count = 
+			}
+		} else {
+			$this -> entity_name = null;
+		}
 	}
+
 }
-
-
 ?>
