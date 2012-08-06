896baab1a9facf6f113405262218c75154704a5d
Abdullah Almehmadi <cs.abdullah@gmail.com>
2012-08-07 02:26:37 +0300

    UQLRule.php was modified

--- UQLRule.php
+++ UQLRule.php
@@ -2,8 +2,33 @@
 
 class UQLRule{
 	
+	private $entity_name;
+	private $alises_map;
+	private $rules_map;
+
+    public function __construct($entity_name)
+	{
+		$this->entity_name = $entity_name;
+		$this->alises_map  = new UQLMap();
+		$this->rules_map   = new UQLMap();
+	}
 	
+	public function __call($function_name,$parameters)
+	{
+		
+	}
 }
 
 //function uql_rule_isemail($name,$value,$alias = null,&$result = null) {}
+//function uql_rule_between($name,$value,$params = null,$alias = null,&$result = null) {}
+
+$the_students_rules = new UQLRule("studnets");
+
+$the_students_rules->id('number');
+$the_students_rules->name('length',50);
+$the_students_rules->email('email');
+$the_students_rules->name('between',10,50);
+
+
+
 ?>
