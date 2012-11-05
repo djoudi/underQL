def701f8db55cf33bc47a8d569ffc0b303fc4631
Abdullah Eid <cs.almehmadi@gmail.com>
2012-11-05 16:33:50 +0300

    Modify insertion class

--- UQLInsertQuery.php
+++ UQLInsertQuery.php
@@ -4,7 +4,8 @@ class UQLInsertQuery{
 	
 	private $query;
 	private $abstract_entity;
-	private $values_map; //used to save list of [field_name = value] that are comming from current insertion query
+	//used to save list of [field_name = value] that are comming from current insertion query
+	private $values_map;
 	
 	public function __construct(&$database_handle,&$abstract_entity)
 	{
@@ -14,7 +15,26 @@ class UQLInsertQuery{
 			$this->query = new UQLQuery($database_handle);
 			$this->abstract_entity = $abstract_entity;
 			$this->values_map = new UQLMap();
-			
+	}
+	
+	public function __set($name,$value)
+	{
+	   if(!$this->abstract_entity->isFieldExist($name))
+	    die($name.' is not a valid column name');
+	    
+	    $this->values_map->addElement($name,$value);
+	}
+	
+	public function __get($name)
+	{
+   	  if(!$this->abstract_entity->isFieldExist($name))
+	    die($name.' is not a valid column name');
+	  
+	  if(!$this->values_map->isElementExist($name))
+	   return null;
+	 else
+	   return $this->values_map->findElement($name);  
+	  
 	}
 	
 }
