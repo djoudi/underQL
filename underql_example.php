73a99ca7b0f358070d82cba79aa5ac7a361d1f14
Abdullah Eid <cs.almehmadi@gmail.com>
2012-11-07 01:59:42 +0300

    underQL was modified

--- underql_example.php
+++ underql_example.php
@@ -3,10 +3,9 @@
 require_once('underQL.php');
 require_once(UQL_DIR_FILTER.'uql_filter_html.php');
 
-//$_('users');
+$_->loadEntity('users');
 
-echo PHP_VERSION;
-echo '<pre>';
-//var_dump($the_users);
-echo '</pre>';
+$result = $the_users->select('*');
+
+echo $result->email;
 ?>
