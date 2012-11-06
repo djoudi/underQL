a1938fef3aff1cab13e40a7674330cee861b275b
Abdullah Eid <cs.almehmadi@gmail.com>
2012-11-07 01:45:09 +0300

    underQL was modified

--- underql_example.php
+++ underql_example.php
@@ -1,26 +1,12 @@
 <?php
 
-
+require_once('underQL.php');
 require_once(UQL_DIR_FILTER.'uql_filter_html.php');
 
-$db = new UQLConnection(UQL_DB_HOST,UQL_DB_NAME,UQL_DB_USER,UQL_DB_PASSWORD,UQL_DB_CHARSET);
-$the_users = new UQLEntity('users',$db);
+//$_('users');
 
-$the_users_filter = new UQLFilter('users');
-$the_users_filter->name('html',UQL_FILTER_OUT,'<h1><b>','</b></h1>');
-
-
-$the_users->id = 5007;
-$the_users->name = 'شبكة عبدالله عيد التعليمية';
-$the_users->email = "cs.abdullah@hotmail.com";
-$the_users->description = "مبرمج ومحب للخير";
-
-$the_users->save();
-
-$_('users');
-
-$the_users
-
-
-//UQLEnvironment // to link all data that releated to different classes // singlton
+echo PHP_VERSION;
+echo '<pre>';
+//var_dump($the_users);
+echo '</pre>';
 ?>
