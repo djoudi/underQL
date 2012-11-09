654f15fbe2f754e5a340bd90dad117b248ff70be
Abdullah Eid <cs.almehmadi@gmail.com>
2012-11-09 19:06:31 +0300

    filters.php and rules.php were removed

--- filters.php
+++ filters.php
@@ -1,3 +1,15 @@
 <?php
 
+
+require_once('filters_api/uql_filter_html.php');
+require_once('filters_api/uql_filter_xss.php');
+
+$users_filter = new UQLFilter('users');
+
+$users_filter->name('xss',UQL_FILTER_IN);
+$users_filter->name('html',UQL_FILTER_OUT);
+$users_filter->email('xss',UQL_FILTER_IN);
+$users_filter->description('html',UQL_FILTER_OUT);
+
+
 ?>
