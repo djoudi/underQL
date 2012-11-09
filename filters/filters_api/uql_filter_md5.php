771946f8bcafd75f4ac7f1091dcb644f49f11a31
Abdullah Eid <cs.almehmadi@gmail.com>
2012-11-09 19:34:19 +0300

    md5 filter was added

--- filters/filters_api/uql_filter_md5.php
+++ filters/filters_api/uql_filter_md5.php
@@ -1,7 +1,5 @@
 <?php
 
-require_once('filters_api/uql_filter_md5.php');
-
 function ufilter_md5($name,$value,$in_out,$params = null)
 {
   return md5($value);
--- filters/users.php
+++ filters/users.php
@@ -2,7 +2,7 @@
 
 require_once('filters_api/uql_filter_md5.php');
 
-//$users_filter = new UQLFilter('users');
-//$users_filter->name('md5',UQL_FILTER_IN);
+$users_filter = new UQLFilter('users');
+$users_filter->name('md5',UQL_FILTER_IN);
 
 ?>
