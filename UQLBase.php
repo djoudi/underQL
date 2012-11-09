3c969a419c49fa3041772b9914ac3a048acf5d9e
Abdullah Eid <cs.almehmadi@gmail.com>
2012-11-09 15:30:00 +0300

    freeResources method was deleted from UQLBase class

--- UQLBase.php
+++ UQLBase.php
@@ -2,8 +2,7 @@
 
 class UQLBase{
 
- public function freeResources(){}
- 
+ //public function freeResources(){}
  public function error($message)
  {
    die('<h3><code><b style = "color:#FF0000">UnderQL Error: </b>'.$message.'</h3>');
