686f49e03f98258fd06d1339240cf2e10032eb7f
Abdullah Eid <cs.almehmadi@gmail.com>
2012-11-10 03:57:58 +0300

    utilities.php was added

--- utilities.php
+++ utilities.php
@@ -3,12 +3,12 @@
 function include_filters()
 {
   $params = func_get_args();
-  
+ 
   if(func_num_args() == 0)
    die('You must pass one filter at least to include_filters');
    
   foreach($params as $key => $filter)
-   require_once(UQL_DIR_FILTER_API.'uql_filter_'.$filter.'.php');
+   require_once(__DIR__.'/'.UQL_DIR_FILTER.UQL_DIR_FILTER_API.'uql_filter_'.$filter.'.php');
 }
 
 function include_rules()
@@ -19,7 +19,7 @@ function include_rules()
    die('You must pass one rule at least to include_rules');
    
   foreach($params as $key => $rule)
-   require_once(UQL_DIR_RULE_API.'uql_rule_'.$rule.'.php');
+   require_once(__DIR__.'/'.UQL_DIR_RULE.UQL_DIR_RULE_API.'uql_rule_'.$rule.'.php');
 }
 
 
