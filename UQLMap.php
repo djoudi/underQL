f3e243094ad42a145f05e94116b40042f6495735
Abdullah Almehmadi <cs.abdullah@gmail.com>
2012-11-20 03:06:55 +0300

    UQLMap was merged

--- UQLMap.php
+++ UQLMap.php
@@ -1,66 +1,5 @@
 <?php
 
-<<<<<<< HEAD
-class UQLMap {
-
-	private $map_list;
-	private $elements_count;
-
-	public function __construct() {
-		$this -> map_list = array();
-		$this -> elements_count = 0;
-	}
-
-	public function addElement($key, $value) {
-		$this -> map_list[$key] = $value;
-		$this -> elements_count++;
-	}
-
-	public function findElement($key) {
-		if ($this -> isElementExist($key))
-			return $this -> map_list[$key];
-
-		return null;
-	}
-
-	public function isElementExist($key) {
-		if ($this -> elements_count <= 0)
-			return false;
-
-		if (@array_key_exists($key, $this -> map_list))
-			return true;
-
-		return false;
-	}
-	
-	public function getCount()
-	{
-		return count($this->map_list);
-	}
-
-	public function removeElement($key) {
-
-		if ($this -> isElementExist($key)) {
-			unset($this -> map_list[$key]);
-			$this -> elements_count--;
-		}
-	}
-
-	public function isEmpty() {
-		return $this -> elements_count == 0;
-	}
-
-	public function mapCallback($callback) {
-		if (!$this -> isEmpty())
-			return array_map($callback, $this -> map_list);
-	}
-
-	public function __destruct() {
-		
-		$this -> map_list = null;
-		$this -> elements_count = 0;
-	}
-=======
 class UQLMap extends UQLBase{
 
     private $uql_map_list;
@@ -126,7 +65,7 @@ class UQLMap extends UQLBase{
         $this -> uql_map_list = null;
         $this -> uql_elements_count = 0;
     }
->>>>>>> c83940c6a6518f85393061fa3e2113e99b6e13ad
+
 
 }
 ?>
