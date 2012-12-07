7079cff1951212097990bb60ea818e5d73042bed
Abdullah Eid <cs.almehmadi@gmail.com>
2012-12-07 18:25:18 +0300

    module system was integrated

---
+++ modules/umodule_demo.php
@@ -0,0 +1,28 @@
+<?php 
+
+
+
+class umodule_demo implements IUQLModule{
+
+ public function init()
+ {
+   echo 'init Demo ..<br />';
+ }
+ 
+ public function in(&$values,$is_insert = true)
+ {
+    echo 'in Demo .. <br />';
+ }
+ 
+ public function out(&$path)
+ {
+    echo 'out Demo .. <br />';
+ }
+ 
+ public function shutdown()
+ {
+   echo 'shutdown Demo..<br />';
+ }
+}
+
+?>
