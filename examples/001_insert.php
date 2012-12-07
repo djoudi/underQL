df862c849c9606acb12ac7c0e8b3f15f9ea00bc6
Abdullah Eid <cs.almehmadi@gmail.com>
2012-12-07 03:00:35 +0300

    comments were added to 001_insert.php

--- examples/001_insert.php
+++ examples/001_insert.php
@@ -7,13 +7,13 @@ Fields : id - name - email - description
 
 require_once('underQL.php');
 
-$_('users');
+$_('users'); // $_('table_name') this will create a new object named $table_name
 
-$users->id = 10;
+$users->id = 10; // ignore this if it is primary and auto_increment
 $users->name = 'Abdullah';
 $users->email = 'cs.abdullah@hotmail.com';
 $users->description = 'Programmer';
 
-$users->_('insert');
+$users->_('insert'); // execute insert command
 
 ?>
