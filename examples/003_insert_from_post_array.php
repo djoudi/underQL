006605c4904d71cfd321b928a2b3b7989b01cb97
Abdullah Eid <cs.almehmadi@gmail.com>
2012-12-07 03:13:11 +0300

    003_insert_from_post_array.php was added to examples

---
+++ examples/003_insert_from_post_array.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+Table name : users
+Fields : id - name - email - description
+*/
+
+require_once('underQL.php');
+
+$_('users'); // $_('table_name') this will create a new object named $table_name
+
+// underQL will try to find id - name - email - description keys and ignore the others.
+$users->_('insert_from_array',$_POST); // you can use $_GET or $_REQUEST if you would or any other array.
+
+?>
