b83dae176d49a50363711e510dcd0b85b354c066
Abdullah Eid <cs.almehmadi@gmail.com>
2012-12-07 15:59:43 +0300

    IUQLModule and module system were built

--- example.php
+++ example.php
@@ -1,7 +1,18 @@
 <?php 
 
 
-require_once('underQL.php');
+//require_once('underQL.php');
 
+define ('UQL_PLUGIN_IN',1);
+define ('UQL_PLUGIN_OUT',2);
+
+
+interface IUQLModule{
+
+  public function init();
+  public function in(UQLMap $values);
+  public function out(UQLQueryPath &$path);
+  public function shutdown();
+}
 
 ?>
