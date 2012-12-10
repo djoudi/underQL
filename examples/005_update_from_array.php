819a8cf9bef77ace9161cc779a1a2187ba8cd1c1
Abdullah Eid <cs.almehmadi@gmail.com>
2012-12-10 08:18:47 +0300

    005_update_from_array.php example was added

---
+++ examples/005_update_from_array.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+Table name : users
+Fields : id - name - email - description
+*/
+
+require_once('underQL.php');
+
+$the_array['id'] = 10;
+$the_array['name'] = 'Abdullah';
+$the_array['email'] = 'cs.abdullah@hotmail.com';
+$the_array['description'] = 'Programmer';
+
+$_('users'); // $_('table_name') this will create a new object named $table_name
+
+// this will update ALL emails and descriptions to the above values
+$users->_('update_from_array',$the_array);
+// or you can yous it as :
+//   $users->_('update_from_array',$the_array,"put extra sql here like where");
+//   $users->_('update_from_array',$the_array,"WHERE $id = 10");
+// NOTE : it is exactly works like $users->_('update'), but it is takes its values
+//         from array.
+
+?>
